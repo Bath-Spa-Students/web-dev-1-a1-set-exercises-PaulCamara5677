c22f0ff345b257a2303d9c4c4201f09bd3cdd517
PaulCamara5677 <144330112+PaulCamara5677@users.noreply.github.com>
2024-04-10 21:55:18 +0400

    Chapter-5-Bonus-Audio-Sampler

---
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/Chapter-5-Audio-sampler.js
@@ -0,0 +1,24 @@
+var mp3 = document.getElementsByClassName('mp3');
+  Array.prototype.forEach.call(mp3, function(element) {
+      element.addEventListener('click', function () {
+          var audioSrc = this.getAttribute('data-src');
+          play(audioSrc); 
+      });
+  });
+
+
+function play(audioSrc) {
+  const audio = new Audio(audioSrc);
+  audio.playbackRate = 1.0; // Set the playback rate
+  audio.play();
+}  
+
+var soundPlayer = new Audio();
+
+function playLowerNote() {
+  soundPlayer.src = audioSrc; // Use the provided audio source
+  soundPlayer.mozPreservesPitch = false;
+  soundPlayer.play();
+}
+
+playLowerNote();
